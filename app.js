91d0a04fa0c07be619e045cae29476bd3e82b334
Shawn Elston <elstonshawn1@yahoo.com>
2021-04-29 15:59:34 -0700

    heroku

--- app.js
+++ app.js
@@ -10,18 +10,14 @@ app.use(express.urlencoded({
 }));
 
 app.use('/', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+
   res.send('<h1>Welcome back</h1>')
 });
 
 app.get('/', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+  
   res.send('hello')
 });
 
